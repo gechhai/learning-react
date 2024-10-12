67320a76978cc1d94860121368f90c25238ba0b1
gechhai <theagechhai@gmail.com>
2024-10-12 19:55:47 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const InputField = ({ addItems }) => {
+  const [todoInput, setTodoInput] = useState("");
+
+  const handleButtonClick = () => {
+    addItems(todoInput);
+    setTodoInput("");
+  };
+
+  return (
+    <div>
+      <input
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+        type="text"
+        placeholder="Add a new task"
+      />
+      <button className="btn btn-primary" onClick={handleButtonClick}>Add</button>
+    </div>
+  );
+};
+
+// React Seperate HTML into small components
+// React Components are functions that return JSX
+
+const users = {
+  name: "John",
+  role: "Instructor",
+};
+
+const ListItem = ({ title }) => {
+  // const { title } = props;  => ({ title })so basically we replace the parameter props with {title} because title is actually a prop.
+  return <p>{title}</p>;
+};
+
+// const ListItem = ( props ) => {
+//   // const { title } = props;  => ({ title })so basically we replace the parameter props with {title} because title is actually a prop.
+//   return (
+//     <p>{props.title}</p>
+//   )
+// };
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search for a task" />
+    </div>
+  );
+};
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const savedTodos = localStorage.getItem("listItem");
+    if (savedTodos) {
+      setTodoList(savedTodos.split(","));
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("Update Local storage");
+    if (todoList.length !== 0){
+      localStorage.setItem("listItem", todoList);
+    }
+  }, [todoList])
+
+
+  const addItems = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <InputField addItems={addItems} />
+      {todoList.map((todo) => {
+        return <ListItem title={todo} />;
+      })}
+      {/* < ListItem title= "Go School"/> */}
+      <SearchBar />
     </div>
   );
 }
 
 export default App;
+addItems
